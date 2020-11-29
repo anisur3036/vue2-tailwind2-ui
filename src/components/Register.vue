ea609e1d0c063302794c76219802aa4b8448d46e
Anisur Rahman <anisurlist3@gmail.com>
2020-11-29 22:45:12 +0600

    working with validation for registration

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -11,7 +11,7 @@
           <span class="text-gray-700 text-sm">Your name</span>
           <input
             :class="
-              errors[0]
+              errors.name
                 ? 'border-red-300 focus:ring-red-300 focus:red-opacity-50'
                 : 'border-green-300 focus:ring-green-300 focus:green-opacity-50'
             "
@@ -19,36 +19,21 @@
             class="py-1 px-2 mt-1 border block w-full rounded-md focus:ring-1"
             v-model="form.name"
           />
-          <icon
-            v-if="!errors[0]"
-            name="check"
-            class="w-4 h-4 absolute text-green-300 top-0 right-0 mt-8 mr-2 z-20"
-          />
-          <icon
-            v-if="errors[0]"
-            name="close"
-            class="w-4 h-4 absolute top-0 text-red-300 right-0 mt-9 mr-2 z-20"
-          />
+          <p v-if="errors.name">{{ errors.name }}</p>
         </label>
         <label class="relative block">
           <span class="text-gray-700 text-sm">Email</span>
           <input
             type="email"
             :class="
-              errors[0]
+              errors.email
                 ? 'border-red-300 focus:ring-red-300 focus:red-opacity-50'
                 : 'border-green-300 focus:ring-green-300 focus:green-opacity-50'
             "
             class="py-1 px-2 mt-1 border border-green-200 block w-full rounded-md focus:ring-1 focus:ring-green-200 focus:green-opacity-50"
             v-model="form.email"
           />
-          <template v-if="!errors">
-            <icon
-              v-show="errors[0]"
-              name="close"
-              class="w-4 h-4 absolute top-0 text-red-300 right-0 mt-9 mr-2 z-20"
-            />
-          </template>
+          <p v-if="errors.email">{{ errors.email }}</p>
         </label>
 
         <label class="block mt-3">
@@ -56,7 +41,7 @@
           <input
             type="password"
             :class="
-              errors[0]
+              errors && errors[0]
                 ? 'border-red-300 focus:ring-red-300 focus:red-opacity-50'
                 : 'border-green-300 focus:ring-green-300 focus:green-opacity-50'
             "
@@ -98,10 +83,10 @@
 </template>
 
 <script>
-import Icon from "@/shared/Icon";
+// import Icon from "@/shared/Icon";
 export default {
   components: {
-    Icon
+    //Icon
   },
   data() {
     return {
@@ -111,12 +96,12 @@ export default {
         password: null,
         password2: null
       },
-      errors: []
+      errors: {}
     };
   },
   methods: {
     register() {
-      this.errors = [];
+      this.errors = {};
       if (
         this.form.name &&
         this.form.email &&
@@ -127,21 +112,21 @@ export default {
       }
 
       if (!this.form.name) {
-        this.errors.push({ name: "Name is required." });
-        console.log(this.errors[0].name);
+        let name = "Name is Required";
+        this.errors = { ...this.errors, name };
       }
-      if (!this.form.email) {
-        this.errors.push({ email: "Email is required." });
-        console.log(this.errors[0].email);
-      }
-
       if (
         /^([a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]{2,})+@([a-zA-Z0-9-]{2,})+(?:\.([a-zA-Z0-9-]{2,6}))*$/.test(
           this.form.email
         ) === false
       ) {
-        this.errors.push({ email: "Email is not valid." });
-        console.log(this.errors[0].email);
+        let email = "Email is not valid.";
+        this.errors = { ...this.errors, email };
+      }
+
+      if (!this.form.email) {
+        let email = "Email is Required";
+        this.errors = { ...this.errors, email };
       }
     }
   }
